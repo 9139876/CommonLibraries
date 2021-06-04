7b220e652b3d014f9618c7f010c682ad919875b8
9139876 <9139876@gmail.com>
2021-06-04 21:26:37 +0300

    Upgrade Json serialize - add serialize object with abstract properties.

--- Core/Extensions/CommonExtensions.cs
+++ Core/Extensions/CommonExtensions.cs
@@ -7,7 +7,7 @@ namespace CommonLibraries.Core.Extensions
 {
     public static class CommonExtensions
     {
-        private static JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
+        private static readonly JsonSerializerSettings _jsonSerializerDefaultSettings = new JsonSerializerSettings
         {
             Converters = new List<JsonConverter> { new StringEnumConverter() },
             ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
@@ -36,12 +36,44 @@ namespace CommonLibraries.Core.Extensions
 
         public static string Serialize(this object value)
         {
-            return JsonConvert.SerializeObject(value, JsonSerializerSettings);
+            var jsonSerializerSettings = _jsonSerializerDefaultSettings;
+            if(IfContainAbstractMembers(value.GetType()))
+            {
+                jsonSerializerSettings.TypeNameHandling = TypeNameHandling.All;
+            }
+
+            return JsonConvert.SerializeObject(value, jsonSerializerSettings);
         }
 
         public static T Deserialize<T>(this string value)
         {
-            return JsonConvert.DeserializeObject<T>(value, JsonSerializerSettings);
+            var jsonSerializerSettings = _jsonSerializerDefaultSettings;
+            if (IfContainAbstractMembers(value.GetType()))
+            {
+                jsonSerializerSettings.TypeNameHandling = TypeNameHandling.All;
+            }
+
+            return JsonConvert.DeserializeObject<T>(value, jsonSerializerSettings);
+        }
+
+        private static bool IfContainAbstractMembers(Type type)
+        {
+            foreach (var property in type.GetProperties())
+            {
+                var propertyType = property.PropertyType;
+
+                if (propertyType.IsAbstract || propertyType.IsInterface)
+                {
+                    return true;
+                }
+
+                if (IfContainAbstractMembers(propertyType))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
