ff00f6f6f7e782b7e99d6a7afafff55b10da83ff
9139876 <9139876@gmail.com>
2021-11-06 23:56:00 +0300

    Add override ToString for TickerAndMarket.

--- CommonLibraries.Graal/Models/TickerAndMarket.cs
+++ CommonLibraries.Graal/Models/TickerAndMarket.cs
@@ -2,10 +2,19 @@
 
 namespace CommonLibraries.Graal.Models
 {
+    /// <summary>
+    /// Универсальный идентификатор инструмента
+    /// </summary>
     public class TickerAndMarket
     {
+        /// <summary>
+        /// Имя рынка
+        /// </summary>
         public string MarketName { get; set; }
 
+        /// <summary>
+        /// Имя инструмента
+        /// </summary>
         public string TickerName { get; set; }
 
         public override bool Equals(object obj)
@@ -28,6 +37,11 @@ namespace CommonLibraries.Graal.Models
         public static bool operator !=(TickerAndMarket left, TickerAndMarket right)
         {
             return !(left == right);
+        }
+
+        public override string ToString()
+        {
+            return $"{TickerName} рынок {MarketName}";
         }
     }
 }
