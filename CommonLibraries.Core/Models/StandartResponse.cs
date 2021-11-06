75266469b3fdddf7571905e9a13ba704e42407fd
9139876 <9139876@gmail.com>
2021-11-07 00:17:31 +0300

    Add StandartResponse model.

---
+++ CommonLibraries.Core/Models/StandartResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CommonLibraries.Core.Models
+{
+    public class StandartResponse
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+    }
+}
