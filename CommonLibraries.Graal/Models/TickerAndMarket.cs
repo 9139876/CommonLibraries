34a4e5e7d2ac1711e73aab16ad88eeaf410547a2
9139876 <9139876@gmail.com>
2021-11-06 22:24:34 +0300

    Add tickerAndMarket model.

---
+++ CommonLibraries.Graal/Models/TickerAndMarket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CommonLibraries.Graal.Models
+{
+    public class TickerAndMarket
+    {
+        public string MarketName { get; set; }
+
+        public string TickerName { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is TickerAndMarket tickerAndMarket &&
+                   MarketName == tickerAndMarket.MarketName &&
+                   TickerName == tickerAndMarket.TickerName;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(MarketName, TickerName);
+        }
+
+        public static bool operator ==(TickerAndMarket left, TickerAndMarket right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(TickerAndMarket left, TickerAndMarket right)
+        {
+            return !(left == right);
+        }
+    }
+}
