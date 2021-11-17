7cf799537f2f6eebf3ddb1932b9a310887abc339
9139876 <9139876@gmail.com>
2021-11-17 22:54:55 +0300

    Add PriceTime model.

---
+++ CommonLibraries.Graal/Models/PriceTime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CommonLibraries.Graal.Models
+{
+    public class PriceTime
+    {
+        public decimal Price { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
